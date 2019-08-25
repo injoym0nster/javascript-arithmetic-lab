5bd0a42e40732e3ca348a018d65773002805dcec
Sahduddin ahmed <53123532+injoym0nster@users.noreply.github.com>
2019-08-25 11:20:40 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+make room ladies for my uktimate smell proof manlly odar after shave
+did we just lose money?
+in the which business?
+
+im not so good whith the programming language.
+//break n coffee
